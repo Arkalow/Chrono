b26df1b85d264032698814129135ab0037b4f5ec
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-02-02 10:27:08 +0100

    Ajout du constructeur de Tast

--- app/src/main/java/fr/iutamiens/lakraao/compte/Task.java
+++ app/src/main/java/fr/iutamiens/lakraao/compte/Task.java
@@ -1,12 +1,30 @@
 package fr.iutamiens.lakraao.compte;
 
+import android.content.Context;
 import android.os.AsyncTask;
+import android.widget.TextView;
 
 /**
  * Created by omer on 02/02/18.
  */
 
 public class Task extends AsyncTask {
+
+    /*
+    Champs
+     */
+    private final Context context;
+
+    private final TextView textView;
+
+    /*
+    Constructeur
+     */
+    public Task(Context context, TextView text){
+        this.context = context;
+        this.textView = text;
+    }
+
     @Override
     protected Object doInBackground(Object[] objects) {
         return null;
