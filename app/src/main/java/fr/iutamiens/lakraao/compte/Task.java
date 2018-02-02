522cc2f6e1220ceccafff695ad47bfeb61cea39a
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-02-02 10:20:58 +0100

    Ajout de la classe Task

---
+++ app/src/main/java/fr/iutamiens/lakraao/compte/Task.java
@@ -0,0 +1,34 @@
+package fr.iutamiens.lakraao.compte;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by omer on 02/02/18.
+ */
+
+public class Task extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+    }
+
+    @Override
+    protected void onProgressUpdate(Object[] values) {
+        super.onProgressUpdate(values);
+    }
+
+    @Override
+    protected void onCancelled() {
+        super.onCancelled();
+    }
+}
