4a3b22586a745748a70a14629323ce7cb7d4362e
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-02-02 10:14:10 +0100

    Ajout de l'input et du bouton avec sont click

--- app/src/main/java/fr/iutamiens/lakraao/compte/MainActivity.java
+++ app/src/main/java/fr/iutamiens/lakraao/compte/MainActivity.java
@@ -2,12 +2,29 @@ package fr.iutamiens.lakraao.compte;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button bt_toogle;
+    private EditText txt_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt_toogle = findViewById(R.id.toogle);
+        txt_input = findViewById(R.id.input_time);
+        
+        bt_toogle.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Log.d("click", "ok");
+            }
+        });
+
     }
 }
